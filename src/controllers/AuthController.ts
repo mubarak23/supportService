9e072b40fa3cdb7d4fddb48fd0e0ebd852b49f42
mubarak23 <mubarakaminu340@gmail.com>
2022-09-30 17:03:49 +0100

    auth signin for agent

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -8,14 +8,15 @@ import * as Utils from "../utils/core"
 import * as TokenService from "../services/tokenService";
 import { IAgentSignupDto } from "../dto/IAgentSignupDto";
 import { IAccessTokenData } from "../interfaces/IAccessTokenData";
-import { BadRequestError } from "../utils/error-response-types";
+import { BadRequestError, UnauthorizedRequestError } from "../utils/error-response-types";
+import { ILoginDto } from "../dto/ILoginDto";
 
 
 @Route("/api/auth")
 @Tags("Auth Service")
 export class AuthController {
 
-@Post("/agent/singup")
+@Post("/agent/signup")
 public async agentSignup(@Body() reqBody: IAgentSignupDto) : Promise<IServerResponse<IAccessTokenData>>{
     const connection = await getFreshConnection()
     const userRepo = connection.getRepository(User)
@@ -24,7 +25,7 @@ public async agentSignup(@Body() reqBody: IAgentSignupDto) : Promise<IServerResp
     if(exitingUser){
     throw new BadRequestError("The Email Address and Phone Number has been used")
     }
-    const passwordHash = await Utils.generatePasswordHash(reqBody.passwod)
+    const passwordHash = await Utils.generatePasswordHash(reqBody.password)
     const newUser = new User().initialize(
         reqBody.firstName, reqBody.lastName, reqBody.emailAddress, reqBody.phoneNumber, passwordHash
     )
@@ -39,5 +40,29 @@ public async agentSignup(@Body() reqBody: IAgentSignupDto) : Promise<IServerResp
     return resData
 
 }
-    
+
+@Post("/agent/signin")
+public async agentSignin(@Body() reqBody: ILoginDto) : Promise<IServerResponse<IAccessTokenData>>{
+    const {emailAddress, password} = reqBody
+    const connection = await getFreshConnection()
+    const userRepo = connection.getRepository(User)
+    const doesUserExist = await userRepo.findOne({ emailAddress })
+    if(!doesUserExist){
+        throw new BadRequestError('Invalid Login credentials')
+    }
+    const match = await bcrypt.compare(password, doesUserExist.passwordHash);
+    if (!match) {
+      throw new UnauthorizedRequestError("User credentials are wrong.");
+    }
+
+    const signinToken = await TokenService.getAccessToken(doesUserExist)
+    const resData : IServerResponse<IAccessTokenData> = {
+        status: true,
+        data:signinToken,
+        message: 'Login Successfully' 
+    }
+    return resData
+
+}
+
 }
--- src/dto/IAgentSignupDto.ts
+++ src/dto/IAgentSignupDto.ts
@@ -3,5 +3,5 @@ export interface IAgentSignupDto {
     lastName: string;
     emailAddress: string;
     phoneNumber: string;
-    passwod: string
+    password: string
 }
